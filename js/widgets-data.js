22db5b21b365bd44eb5c4a5f151915609a044d79
Lakhan <lakhan_m@tekditechnologies.com>
2018-08-16 23:34:14 +0530

    Deploy Docs - Build No. 246

--- js/widgets-data.js
+++ js/widgets-data.js
@@ -4,7 +4,7 @@ var widgetsData = {
          "title":"2.0",
          "desc":"Desc of 2.0",
          "releaseDate":"14th Aug",
-         "latest":false,
+         "latest":true,
          "published":true
       },
       {  
@@ -12,20 +12,6 @@ var widgetsData = {
          "desc":"Desc of 1.9",
          "releaseDate":"14th July",
          "latest":false,
-         "published":true
-      },
-      {  
-         "title":"1.8",
-         "desc":"Desc of 1.8",
-         "releaseDate":"14th June",
-         "latest":false,
-         "published":true
-      },
-      {  
-         "title":"1.7",
-         "desc":"Desc of 1.7",
-         "releaseDate":"14th May",
-         "latest":true,
          "published":true
       }
    ]
