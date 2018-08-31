bfd560e2aaa86b644610f2012ce7a6c21a2c2a3f
Lakhan <lakhan_m@tekditechnologies.com>
2018-08-31 17:29:25 +0530

    Deploy Docs - Build No. 500

--- js/widgets.js
+++ js/widgets.js
@@ -4,9 +4,10 @@ publishedVersions = widgetsData.versions.filter(function(version){
 latestVersion = widgetsData.versions.filter(function(version){
 	return version.published === true && version.latest === true ;
 });
+currentVersionData = widgetsData.versions.filter(function(version){
+	return version.title == version;
+});
 
 $(function(){
 	$("#templates").load("/js/templates.html"); 
 });
-
-
