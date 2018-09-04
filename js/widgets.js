e88e65602767a9b73b7ef5ae31d56c253ec495ba
Lakhan <lakhan_m@tekditechnologies.com>
2018-09-04 10:18:49 +0530

    Deploy Docs - Build No. 518

--- js/widgets.js
+++ js/widgets.js
@@ -4,7 +4,8 @@ publishedVersions = widgetsData.versions.filter(function(version){
 latestVersion = widgetsData.versions.filter(function(version){
 	return version.published === true && version.latest === true ;
 });
-currentVersionData = widgetsData.versions.filter(function(version,currentVersion){
+currentVersion = $('#docsVersion').attr("content");
+currentVersionData = widgetsData.versions.filter(function(version){
 	return version.title == currentVersion;
 });
 
