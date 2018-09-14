9cf81f138b1f289dc2b131ad18d0ff5616a03db1
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-09-14 17:51:51 +0530

    toc update

--- js/widgets-data.js
+++ js/widgets-data.js
@@ -1,23 +1,23 @@
 var widgetsData = {  
    versions:[  
       {  
-         "title":"1.10",
-         "branch":"1.10",
+         "title":"1.9",
+         "branch":"1.9",
          "desc":"",
          "releaseDate":"30th Aug",
          "latest":true,
          "published":true
       },
       {  
-         "title":"1.9",
-         "branch":"1.9",
+         "title":"1.8",
+         "branch":"1.8",
          "desc":"",
          "releaseDate":"30th July",
          "latest":false,
          "published":true
       },
       {  
-         "title":"1.12 (Unreleased)",
+         "title":"1.10 (Unreleased)",
          "branch":"master",
          "desc":"",
          "releaseDate":"30th July",
