6e6de21184454f019a695bf3651fa0793fc61543
Lakhan <lakhan_m@tekditechnologies.com>
2018-09-17 10:51:38 +0530

    Deploy Docs - Build No. 637

--- js/widgets-data.js
+++ js/widgets-data.js
@@ -1,23 +1,31 @@
 var widgetsData = {  
    versions:[  
-      {  
-         "title":"1.9",
-         "branch":"1.9",
+       {  
+         "title":"1.8",
+         "branch":"1.8",
          "desc":"",
-         "releaseDate":"30th Aug",
-         "latest":true,
+         "releaseDate":"30th June",
+         "latest":false,
          "published":true
       },
       {  
-         "title":"1.8",
-         "branch":"1.8",
+         "title":"1.9",
+         "branch":"1.9",
          "desc":"",
          "releaseDate":"30th July",
          "latest":false,
          "published":true
       },
       {  
-         "title":"1.10 (Unreleased)",
+         "title":"1.10",
+         "branch":"1.10",
+         "desc":"",
+         "releaseDate":"30th Aug",
+         "latest":true,
+         "published":true
+      },
+      {  
+         "title":"Draft",
          "branch":"master",
          "desc":"",
          "releaseDate":"30th July",
