df5ff074c85d8a01fa2ca0a979543c3c666ff577
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-09-18 15:08:27 +0530

    dates change

--- js/widgets-data.js
+++ js/widgets-data.js
@@ -4,15 +4,15 @@ var widgetsData = {
          "title":"1.10",
          "branch":"1.10",
          "desc":"",
-         "releaseDate":"30th Aug",
+         "releaseDate":"12th Sept",
          "latest":true,
          "published":true
       },
       {  
-         "title":"1.9",
+         "title":"1.9.1",
          "branch":"1.9",
          "desc":"",
-         "releaseDate":"30th July",
+         "releaseDate":"22nd Aug",
          "latest":false,
          "published":true
       },
@@ -20,7 +20,7 @@ var widgetsData = {
          "title":"1.8",
          "branch":"1.8",
          "desc":"",
-         "releaseDate":"30th June",
+         "releaseDate":"6th July",
          "latest":false,
          "published":true
       },
