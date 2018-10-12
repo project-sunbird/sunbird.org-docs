7ea71a6f6f146d5a9931036d9c59b52e26605c05
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-10-12 11:25:52 +0530

    update

--- js/widgets.js
+++ js/widgets.js
@@ -5,9 +5,7 @@ latestVersion = widgetsData.versions.filter(function(version){
 	return version.published === true && version.latest === true ;
 });
 currentVersion = $('#docsVersion').attr("content");
-currentVersionData = widgetsData.versions.filter(function(currentVersion){
-	return version.title == currentVersion;
-});
+currentVersionData = widgetsData.versions.find( version => version.title === currentVersion);
 console.log(currentVersionData);
 $(function(){
 	$("#templates").load("/js/templates.html"); 
