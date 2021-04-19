7ebbdf1b9a0b470d96e428ce2fc07be7b13f2edb
keshavprasadms <msknext@gmail.com>
2021-04-19 15:22:07 +0530

    Version deploy - 3.8.0 Version & Build No. 3288

--- 3.8.0/automationScripts/proxyvalidator.sh
+++ 3.8.0/automationScripts/proxyvalidator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -eo pipefail
 # Author S M Y ALTAMASH <smy.altamash@gmail.com>
+set -eo pipefail
 
 directoryLocation=$(readlink -f $0| rev | cut -d '/' -f2- | rev)
 cd ${directoryLocation}
 echo -e "Generating the proxy validator yaml by merging all the API definition into 1"
 
 # Install yaml-merge for merging all the yaml files
-npm install -g @alexlafroscia/yaml-merge
+# npm install @alexlafroscia/yaml-merge
 
 completeFile=$(yaml-merge $(find ${directoryLocation}/../apis -name *.yaml | tr "\n" " "))
 
@@ -15,7 +15,7 @@ echo "$completeFile" > completeOpenApiSpecFile.yml
 tmpFile=""
 
 # Patterns to delete from the spec file
-patterns=("servers" "paths.*.*.description" "paths.*.*.operationId" "paths.*.*.tags" "paths.*.*.responses.*.content.*.example" "paths.*.*.responses.*.content.*.examples" "tags" "security" "externalDocs" "paths.*.*.requestBody.content.*.examples" "paths.*.*.requestBody.content.*.example")
+patterns=("servers" "paths.*.*.description" "paths.*.*.operationId" "paths.*.*.tags" "paths.*.*.responses.*.content.*.example" "paths.*.*.responses.*.content.*.examples" "tags" "security" "externalDocs" "paths.*.*.requestBody.content.*.examples" "paths.*.*.requestBody.content.*.example" "info.externalDocs")
 
 # Remove the unwanted fields from the proxyvalidator
 # Now clean the intermediate file
